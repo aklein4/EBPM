56aa271378f5fd2ac55caf0404374b9248dfa998
Adam Klein <adam3207@gmail.com>
2026-01-13 03:35:27 +0000

    gaussian init in PLM

--- src/models/plm/modelling_plm.py
+++ src/models/plm/modelling_plm.py
@@ -1,10 +1,17 @@
 
 from models.plm.configuration_plm import ProteinLMConfig
-from models.custom_llama.modelling_llama import CustomLlamaForCausalLM
+from models.custom_llama.modelling_llama import CustomLlamaForCausalLM, CustomLlamaModel
+
+
+class ProteinTransformer(CustomLlamaModel):
+
+    gaussian_initialization = True
 
 
 class ProteinLM(CustomLlamaForCausalLM):
     
     config: ProteinLMConfig
+
+    transformer_type = ProteinTransformer
 
     gaussian_initialization = True
